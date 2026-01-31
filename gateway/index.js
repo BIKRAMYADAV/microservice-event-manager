f4b21e7899ad31e221b184798ffd1d7e632d5586
BIKRAMYADAV <bikram.deep.yadav.11@gmail.com>
2026-01-31 22:20:21 +0530

    feat: sets up the gateway server

---
+++ gateway/index.js
@@ -0,0 +1,15 @@
+const express = require('express')
+const dotenv = require('dotenv')
+const authProxy = require('./routes/authProxy')
+dotenv.config()
+
+const PORT = process.env.PORT
+const app = express()
+
+app.use(express.json());
+
+app.use("/api/auth", authProxy);
+
+app.listen(PORT, () => {
+    console.log(`The gateway server is listening on port ${PORT}`);
+})
