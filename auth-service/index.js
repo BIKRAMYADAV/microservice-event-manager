4dbb4a9db55559d4ab2afc70a7aa512a313f40a2
BIKRAMYADAV <bikram.deep.yadav.11@gmail.com>
2026-01-31 22:14:58 +0530

    fix: fixes controller issue

--- auth-service/index.js
+++ auth-service/index.js
@@ -1,4 +1,6 @@
 const express = require('express')
+const dotenv = require('dotenv')
+dotenv.config()
 const app = express()
 const connectDB = require("./config/db")
 const AUTHPORT = process.env.AUTHPORT
--- auth-service/routes/authRoutes.js
+++ auth-service/routes/authRoutes.js
@@ -6,6 +6,6 @@ router.post("/register", authController.register)
 
 router.post("/login", authController.login)
 
-router.get("verify", authController.verify);
+router.get("verify", authController.verifyToken);
 
 module.exports = router
