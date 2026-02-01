ffea17b3d3ba5e3b15833a22b2f897944e0c070d
BIKRAMYADAV <bikram.deep.yadav.11@gmail.com>
2026-02-01 12:27:29 +0530

    feat: adds get all events and one event to eventController

--- event-service/controller/eventController.js
+++ event-service/controller/eventController.js
@@ -23,7 +23,11 @@ exports.addEvent = async (req, res) => {
 
 exports.allEvents = async (req, res) => {
     try{
-
+        const events = await Event.find({});
+        res.status(201).json({
+            message: "all events retrieved",
+            events
+        })
     } catch (error){
           console.log('There was an error while getting all event')
         res.status(500).json({
@@ -34,7 +38,18 @@ exports.allEvents = async (req, res) => {
 
 exports.oneEvent = async (req, res) => {
     try{
-
+        const event = await Event.findOne({
+            _id: req.params.id 
+        })
+        if(!event){
+            res.status(400).json({
+                message: 'The event does not exist'
+            })
+        }
+        res.status(201).json({
+            message: "event retrieved",
+            event
+        })
     } catch (error){
           console.log('There was an error while getting one event')
         res.status(500).json({
