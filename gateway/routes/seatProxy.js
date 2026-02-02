4d51827b83fbdf8472d22a14a9214385dfb3c849
BIKRAMYADAV <bikram.deep.yadav.11@gmail.com>
2026-02-02 12:15:13 +0530

    feat: adds seat proxy to gateway

---
+++ gateway/routes/seatProxy.js
@@ -0,0 +1,33 @@
+const express = require('express')
+const router = express.Router()
+const axios = require('axios')
+
+const seatApiUrl = "http://localhost:3003"
+
+router.post("/lock", (req, res) => {
+    try{
+        const response = axios.post(seatApiUrl+"/lock");
+        res.status(response.status).json(response.data);
+    } catch (error){
+        console.log('There was an error at seat gateway')
+        res.status(500);
+    }
+})
+
+// router.post("/confirm", () => {
+//    try{
+
+//     } catch (error){
+//         console.log('There was an error at seat gateway')
+//         res.status(500);
+//     }
+// })
+
+// router.post("/release", () => {
+//    try{
+
+//     } catch (error){
+//         console.log('There was an error at seat gateway')
+//         res.status(500);
+//     }
+// })
--- seat-service/index.js
+++ seat-service/index.js
@@ -3,6 +3,9 @@ const dotenv = require('dotenv')
 dotenv.config()
 const SEATPORT = process.env.SEATPORT
 const app = express()
+const seatRoute = require('./routes/seatRoute')
+
+app.use("/seat",seatRoute);
 
 app.listen(SEATPORT, () => {
     console.log(`The Seat service is running on port ${SEATPORT}`);
