6fa69e106dd40fea4db40192e236eb99262a2457
BIKRAMYADAV <bikram.deep.yadav.11@gmail.com>
2026-02-02 23:28:20 +0530

    feat: adds create Booking feature

--- booking-service/controllers/bookingController.js
+++ booking-service/controllers/bookingController.js
@@ -1,6 +1,37 @@
 const axios = require('axios')
-const seatAPI = ""
+const Booking = require("../models/Booking")
+const SEATAPI = ""
 
 exports.createBooking = async (req, res) => {
-    const seat = await axios.get(seat);
+    try{
+        const {eventId, seatNumber} = req.body;
+        const userId = req.user.id;
+
+        const seatResponse = await axios.get(SEATAPI+`/seats/${eventId}/${seatNumber}`);
+        const seat = seatResponse.data;
+
+        if(seat.status !== "LOCKED"){
+            return res.status(400).json({
+                message: "seat is not locked" 
+            })
+        }
+        const booking = await Booking.create({
+            userId,
+            eventId,
+            seatNumber,
+            status:"PENDING"
+        })
+
+        res.status(201).json({
+            message: "booking created",
+            bookingId: booking._id
+        })
+
+
+    } catch (error){
+        console.log('There was an error in creating a booking')
+        return res.status(500).json({
+            message: "server error"
+        })
+    }
 }
