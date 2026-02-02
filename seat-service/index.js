e22c00a753502b2dff4f269d83e1f72cedc0971a
BIKRAMYADAV <bikram.deep.yadav.11@gmail.com>
2026-02-02 12:41:01 +0530

    feat: sets up mongodb connection in seat service

--- seat-service/index.js
+++ seat-service/index.js
@@ -4,8 +4,13 @@ dotenv.config()
 const SEATPORT = process.env.SEATPORT
 const app = express()
 const seatRoute = require('./routes/seatRoute')
+const connect = require('./config/mongo')
 
+app.use(express.json())
 app.use("/seat",seatRoute);
+
+//mongo connection
+connect()
 
 app.listen(SEATPORT, () => {
     console.log(`The Seat service is running on port ${SEATPORT}`);
