96b810112c937f54f17f19a531636ad011cdec49
BIKRAMYADAV <bikram.deep.yadav.11@gmail.com>
2026-02-04 12:12:48 +0530

    fix: adds booking port

--- booking-service/index.js
+++ booking-service/index.js
@@ -5,10 +5,10 @@ const BOOKINGPORT = process.env.BOOKINGPORT
 const express = require('express')
 const app = express()
 
-spp.use(express.json())
+app.use(express.json())
 app.use("/bookings", require("./routes/bookingRoutes"))
 
 app.listen(BOOKINGPORT, () => {
-    console.log(`The booking service is listening on port ${PORT}`)
+    console.log(`The booking service is listening on port ${BOOKINGPORT}`)
 })
 
