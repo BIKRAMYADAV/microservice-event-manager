fccec56c4a699084089b79eda182b48dcaf29653
BIKRAMYADAV <bikram.deep.yadav.11@gmail.com>
2026-02-04 12:25:06 +0530

    fix: fixes indexjs for microservices

--- booking-service/config/db.js
+++ booking-service/config/db.js
@@ -3,7 +3,7 @@ const mongoose = require("mongoose")
 const connect = async () => {
     try{
         await mongoose.connect(process.env.MONGO_URI);
-        console.log("auth db connected")
+        console.log("booking db connected")
     } catch (error){
         console.log('there was an error while connecting to the db in booking')
     }
--- gateway/index.js
+++ gateway/index.js
@@ -1,6 +1,10 @@
 const express = require('express')
 const dotenv = require('dotenv')
 const authProxy = require('./routes/authProxy')
+const seatProxy = require('./routes/seatProxy')
+const eventProxy = require('./routes/eventProxy')
+const bookingProxy = require('./routes/bookingProxy')
+
 dotenv.config()
 
 const PORT = process.env.PORT
@@ -9,6 +13,9 @@ const app = express()
 app.use(express.json());
 
 app.use("/api/auth", authProxy);
+app.use("/api/seat", seatProxy);
+app.use("/api/booking", bookingProxy);
+app.use("/api/event", eventProxy);
 
 
 app.listen(PORT, () => {
