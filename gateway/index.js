fcb981bd75802a647abf68334e0df7bf6e71d504
BIKRAMYADAV <bikram.deep.yadav.11@gmail.com>
2026-02-07 13:15:49 +0530

    feat: aggregates gateway health

--- gateway/index.js
+++ gateway/index.js
@@ -5,6 +5,7 @@ const seatProxy = require('./routes/seatProxy')
 const eventProxy = require('./routes/eventProxy')
 const bookingProxy = require('./routes/bookingProxy')
 const rateLimiter = require('./middlewares/rateLimiter')
+const healthRoute = require('./routes/health')
 const verifyJwt = require('./middlewares/jwt')
 
 dotenv.config()
@@ -21,13 +22,14 @@ app.use("/api/auth", authProxy);
 app.use("/api/seat",verifyJwt, seatProxy);
 // app.use("/api/booking", bookingProxy);
 // app.use("/api/event", eventProxy);
-app.get("/health", (req, res) => {
-  res.status(200).json({
-    status: "ok",
-    service: "gateway-service",
-    uptime: process.uptime(),
-  });
-});
+// app.get("/health", (req, res) => {
+//   res.status(200).json({
+//     status: "ok",
+//     service: "gateway-service",
+//     uptime: process.uptime(),
+//   });
+// });
+app.use("/health", healthRoute)
 
 app.listen(PORT, () => {
     console.log(`The gateway server is listening on port ${PORT}`);
